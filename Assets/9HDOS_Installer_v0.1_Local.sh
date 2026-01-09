cee2ae2020d117c342bc0cfd9e3befd2d5cc525e
fart-maker <anthengoodwin1@gmail.com>
2026-01-09 05:44:41 -0900

    Add files via upload
    still trying to fix the whiptail

--- Assets/9HDOS_Installer_v0.1_Local.sh
+++ Assets/9HDOS_Installer_v0.1_Local.sh
@@ -181,32 +181,35 @@ gather-customization-info() {
 }
 
 
-# listing disks i think
-select-install-disk ()
-    clear
- disks=$(lsblk -d -o NAME,SIZEE,TYPE | grep disk | awk '{print $1, $2, $3}')
+# select install disk
+select-install-disk() {
+  disks=$(lsblk -d -o NAME,SIZE,TYPE | grep disk | awk '{print $1, $2, $3}')
+}
+
+
  # menu entries and shit
- menu_entries+()
+ menu_entries() {
  while read -r line; do
-    name+$(echo "$line" | awk '{print $1}')
-    size=$(echo "$line" | awek '{$1=""; print $0}' | sed 's/^ //')
+    name=$(echo "$line" | awk '{print $1}')
+    size=$(echo "$line" | awk '{$2}' | sed 's/^ //')
     menu_entries+=("$name" "$size")
 done <<< "$disks"
+ }
 
 #some whiptail that ill never see
+selected_disk(){
 selected_disk=$(whiptail --title "select disk" --menu "choose disk to format"
 20 20 5 "${menu_entries[@]}" 3>&1 1>&2 2>&3)
-
-if [ -n "selected_disk" ]; then
-
 
 #warning
 
 if (whiptail --yesno "are you sure you want to format /dev/$selected_disk?" 10 50); then
-echo "formatting /dev/$selected_disk... please wait"
+echo "formatting /dev/$selected_disk... please wait":
 sudo mkfs ext4 /dev/$selected_disk;
+else
+echo "install failed."
 fi
-
+}
    
 
 
