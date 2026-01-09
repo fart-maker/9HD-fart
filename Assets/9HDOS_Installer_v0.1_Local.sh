3150d3bf688ab6c4277ac3a976a8d0d846313784
fart-maker <anthengoodwin1@gmail.com>
2026-01-08 15:36:34 -0900

    Update 9HDOS_Installer_v0.1_Local.sh

--- Assets/9HDOS_Installer_v0.1_Local.sh
+++ Assets/9HDOS_Installer_v0.1_Local.sh
@@ -181,10 +181,7 @@ gather-customization-info() {
 }
 
 
-# listing disks i think
-select-install-disk ()
-    clear
- disks=$(lsblk -d -o NAME,SIZEE,TYPE | grep disk | awk '{print $1, $2, $3}')
+
  # menu entries and shit
  menu_entries+()
  while read -r line; do
